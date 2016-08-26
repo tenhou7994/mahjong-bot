6ec43f78f02063c0e75e78dfdd17a2e052b2363a
haufau <fgsfds789@gmail.com>
2016-08-26 21:04:04 +0300

    fixes and refactoring

--- router.rb
+++ router.rb
@@ -1,8 +1,17 @@
 # encoding: utf-8
+require_relative 'modules'
 
 class Router
   attr_accessor :default_chat_id
   attr_reader :logger
+
+  include Stat_mj
+  include Links_mj
+  include Schedule_mj
+  include Highlights_mj
+  include Hints_mj
+  include Users_mj
+  include Help_mj
 
   def initialize(db)
     @db = db
@@ -22,49 +31,75 @@ class Router
     @user = message['from']
     @message_text = message['text']
     case @message_text
-      when /\/hlon\s?/
+      when /^\/hlon\s?$/
         set_highlights_on
 
-      when /\/hloff\s?.*/
+      when /^\/hloff(\s.*)?$/
         set_highlights_off
 
-      when /\/mahjong\s?.*/
+      when /^\/mahjong(\s.*)?$/
         call_to_arms
 
-      when /\/me\s.*/
+      when /^\/me\s.+$/
         bind_name
 
       when /^\/stat3\s?$/
         count_self_stat 3
 
-      when /\/stat3\s(\d{1,4}|\w*)(\s.*\s?)?/
+      when /^\/stat3\s(\d{1,4}|\w*)(\s.*\s?)?$/
         count_hard_stat $1, $2, 3
 
       when /^\/stat\s?$/
         count_self_stat 4
 
-      when /\/stat\s(\d{1,4}|\w*)(\s.*\s?)?/
+      when /^\/stat\s(\d{1,4}|\w*)(\s.*\s?)?$/
         count_hard_stat $1, $2, 4
 
-      when /\/whois\s.*/
+      when /^\/whois\s.+$/
         say_who
 
-      when /\/set_sch\s\d?\s?\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}/
+      when /^\/whoami$/
+        who?
+
+      when /^\/set_sch\s\d?\s?\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}$/
         set_user_sch
 
-      when /\/rm_sch\s\d/
+      when /^\/rm_sch\s\d$/
         rm_user_sch
 
-      when /\/my_sch\s?.*/
+      when /^\/my_sch(\s.+)?$/
         send_user_sch
 
-      when /\/help\s?.*/
+      when /^\/help(\s.+)?$/
         send_help
 
-      when /\/list/
-        if @user['id'] == 53783180
-          get_users_from_db
-        end
+      when /^\/help_admin(\s.+)?$/
+        send_admin_help if user_admin?
+
+      when /^\/list$/
+        get_users_from_db if user_admin?
+
+      when /^\/links$/
+        get_links
+
+      when /^\/add_link(\s\d+)?(\s(https?:\/\/)?([\-\w\.]+)\.([\-\da-z]{2,20}\.?)(\/[\w\.]*)*\/?(.*)?)$/
+        add_link $1, $2 if user_admin?
+
+      when /^\/link_desc(\s\d+)(\s.+)$/
+        add_desc $1, $2 if user_admin?
+
+      when /^\/swap_link(\s\d+)(\s\d+)$/
+        swap_link $1, $2 if user_admin?
+
+      when /^\/rm_link(\s\d+)?$/
+        rm_link $1 if user_admin?
+
+      when /^\/hint\s.+/
+        add_hint if user_admin?
+
+      when /^\/how_to_win_maajan(\s.+)?$/
+        get_hint
+
       else
     end
   rescue => err
@@ -100,155 +135,13 @@ class Router
     end
   end
 
-  def set_highlights_on
-    if @user['username'].nil?
-      text = 'Please set your username in telegram profile.'
-      send_message text
-    else
-      if @db.get_users(c_id: @user['id']).size > 0
-        @db.update_user @user['id'], username: @user['username'], hl_s: 1
-      else
-        @db.add_user @user['id'], username: @user['username']
-      end
-    end
-  end
-
-  def set_highlights_off
-    if @user['username'].nil?
-      text = 'You have no username, highlights disabled by default.'
-      send_message text
-    else
-      if @db.get_users(c_id: @user['id']).size > 0
-        @db.update_user @user['id'], username: @user['username'], hl_s: 0
-      else
-        @db.add_user @user['id'], @user['username'], hl_s: 0
-      end
-    end
-  end
-
-  def call_to_arms
-    text = "@#{@user['username']} призывает нажать кнопку в лобби tenhou.net/0/?L7994 (tenhou.net/3/beta.html?L7994) \n"
-    @db.get_users(hl_s: 1).each do |row|
-      if in_interval? row[1]
-        text += "@#{row[0]} | "
-      end
-    end
-    send_message text
-  end
-
-  def bind_name
-    name = @message_text.gsub(/\/me\s/, '')
-    if @db.get_users(c_id: @user['id']).size > 0
-      @db.update_user @user['id'], username: @user['username'], t_id: name
-    else
-      if @user['username'].nil?
-        @db.add_user @user['id'], username: @user['username'], t_id: name, hl_s: 0
-      else
-        @db.add_user @user['id'], username: @user['username'], t_id: name
-      end
-    end
-    text = "Никнейм #{name} на tenhou ассоциирован с Вами. Теперь Вы можете воспользоваться командой /stat {lobby_number}"
-    send_message text
-  end
-
-  def count_self_stat(man)
-    user = @db.get_users(c_id: @user['id'])
-    if user.size > 0
-      player = user[0][1]
-      text = get_stat player, man
-    else
-      text = 'You have no tenhou name binded to you. Use /me {tenhou_name} command first.'
-    end
-    send_message text
-  end
-
-  def count_hard_stat(lobby_string, player, man)
-    if player.nil? or player.gsub!(/^\s/, '').gsub!(/\s$/, '') == ''
-      u_name = @db.get_users(c_id: @user['id'])
-      u_name.size > 0 ? player = u_name[0][1] : player = nil
-    end
-    lobby_string =~ /\d{1,4}/ ? lobby = lobby_string : dan_check = lobby_string
-    dan_strings = %w(general dan perdan upperdan gigadan phoenix alldan)
-    if player and (dan_check.nil? or dan_strings.include? dan_check)
-      text = get_stat(player, man, lobby: lobby, dan: dan_check)
-      send_message text
-    end
-  end
-
-  def say_who
-    t_name = @message_text.gsub(/\/whois\s/, '')
-    u_name = @db.get_users(t_id: t_name)
-    if u_name.size > 0
-      text = "#{t_name} известен как"
-      u_name.each do |row|
-        text += " @#{row[0]}|"
-      end
-    else
-      text = "Я не знаю кто это. Если вы опознали себя, воспользуйтесь командой '/me #{t_name}'."
-    end
-    send_message text
-  end
-
-  def set_user_sch
-    dow = @message_text.gsub(/\/set_sch\s/, '').gsub(/\s?\d{1,2}:\d{1,2}\s?-.*/, '')
-    dow = dow == '' ? 0 : dow
-    if dow.to_i > 7 or dow.to_i < 0
-      text = "Invalid day of the week (1-7, 0 is default for whole week). @#{@user['username']}"
-      send_message text
-      return
-    end
-    time_from = @message_text.gsub(/\/set_sch\s(\d\s|)/, '').gsub(/\s?-\s?\d{1,2}:\d{1,2}/, '')
-    time_to = @message_text.gsub(/\/set_sch\s(\d\s|)\d{1,2}:\d{1,2}\s?-\s?/, '')
-    if @db.get_schedule(chat_id: @user['id'], dow: dow).size > 0
-      @db.update_schedule chat_id: @user['id'], dow: dow, time_from: time_from, time_to: time_to
-    else
-      @db.add_schedule chat_id: @user['id'], dow: dow, time_from: time_from, time_to: time_to
-    end
-  end
-
-  def rm_user_sch
-    dow = @message_text.gsub(/\D/, '')
-    if dow.to_i > 7 or dow.to_i < 0
-      text = "Invalid day of the week (1-7, 0 is default for whole week). #{@user['username']}"
-      send_message text
-      return
-    end
-    if @db.get_schedule(chat_id: @user['id'], dow: dow).size > 0
-      @db.delete_schedule chat_id: @user['id'], dow: dow
-      text = "Schedule on day #{dow} of the week removed. @#{@user['username']}"
-      if dow == 0
-        text = "Default highlight schedule removed. You will be highlighted until /hloff if not already hloff'ed. @#{@user['username']}"
-      end
-    elsif dow != 0
-      text = "You have no schedule on this day already. @#{@user['username']}"
-    else
-      text = "You have no default schedule. You will be highlighted until /hloff if not already hloff'ed. @#{@user['username']}"
-    end
-    send_message text
-  end
-
-  def send_user_sch
-    text = 'Your schedule of highlights (0 is default) :'
-    (0..7).to_a.each do |dow|
-      row = @db.get_schedule chat_id: @user['id'], dow: dow
-      if row.size == 0
-        text += "\n#{dow}: not set yet"
-      else
-        text += "\n#{dow}: #{row[0][0]} - #{row[0][1]}"
-      end
-    end
-    send_message text
-  end
-
-  def send_help
-    text = "Available commands:\n/hlon -- enable notify by /mahjong\n/hloff -- disable notify by /mahjong\n/me {tenhou-name} -- bind tenhou name to you\n/stat {(general/dan/upperdan/gigadan/alldan) | (lobby_number)} -- get stat for you in lobby (/me required)\n"
-    text += "/set_sch (1-7) {HH:MM - HH:MM} -- set (day of week and) time for highlighting by /mahjong\n/my_sch -- get your schedule of highlighting."
-    send_message text
-  end
-
-  def send_message(text, chat: nil)
-    chat ||= @chat['id']
-    RestClient.get "https://api.telegram.org/bot#{BOT_TOKEN}/sendMessage", {:params => {:chat_id => chat, :text => text}}
+  def get_admins
+    chat = @chat['id'] > 0 ? GROUP_CHAT_ID : @chat['id']
+    resp = RestClient.get "https://api.telegram.org/bot#{BOT_TOKEN}/getChatAdministrators",
+                   {:params => {:chat_id => chat}}
+    result = (JSON.parse resp)['result']
+    result.collect! { |user| user['user']['id'] }
+    result
   end
 
   def get_users_from_db
@@ -257,6 +150,19 @@ class Router
       text += "@#{row[0]}\n"
     end
     send_message text
+  end
+
+  def user_admin?
+    @logger.info "Check user #{@user['id']}"
+    get_admins.include? @user['id']
+  end
+
+  def send_message(text, chat: nil, formatted: nil)
+    chat ||= @chat['id']
+    params_hash = {:chat_id => chat, :text => text, :disable_web_page_preview => 1}
+    params_hash.merge!({:parse_mode => 'HTML'}) if formatted
+    RestClient.get "https://api.telegram.org/bot#{BOT_TOKEN}/sendMessage",
+                   {:params => params_hash}
   end
 
 end
--- sqlpart.rb
+++ sqlpart.rb
@@ -14,8 +14,12 @@ class DBC
       (chat_id int unique, username text, tenhou_id text, hl_status boolean);"
     query_schedule = "create table if not exists schedules
       (chat_id int, d_of_w int, time_from text, time_to text)"
+    query_links = "create table if not exists links(id int unique, link text, desc text);"
+    query_miko_hints = 'create table if not exists hints(hint text unique);'
     @db.execute query_main
     @db.execute query_schedule
+    @db.execute query_links
+    @db.execute query_miko_hints
   end
 
   def add_user(c_id, username: ,t_id: nil , hl_s: 1)
@@ -66,6 +70,70 @@ class DBC
 
   def get_schedule(chat_id:, dow: 0)
     query = "select time_from, time_to from schedules where chat_id = #{chat_id} and d_of_w=#{dow};"
+    @db.execute query
+  end
+
+  def add_link(id, link)
+    desc = 'no description'
+    query = "insert or replace into links values (#{id}, '#{link}', '#{desc}');"
+    @db.execute query
+  end
+
+  def add_link_desc(id, desc)
+    query = "update links set desc='#{desc}' where id=#{id};"
+    @db.execute query
+  end
+
+  def update_link_id(id)
+    query = "update links set id=#{id-1} where id=#{id};"
+    @db.execute query
+  end
+
+  def get_link(id)
+    query = "select id from links where id=#{id};"
+    @db.execute query
+  end
+
+  def get_links
+    query = 'select * from links order by id asc;'
+    @db.execute query
+  end
+
+  def get_last_link_id
+    query_last_index = 'select max(id) from links;'
+    @db.get_first_value query_last_index
+  end
+
+  def swap_links(id_1, id_2)
+    temp_id = get_last_link_id + 1000
+    query_swap_1 = "update links set id=#{temp_id} where id=#{id_2};"
+    query_swap_2 = "update links set id=#{id_2} where id=#{id_1};"
+    query_swap_3 = "update links set id=#{id_1} where id=#{temp_id};"
+    @db.execute query_swap_1
+    @db.execute query_swap_2
+    @db.execute query_swap_3
+  end
+
+  def delete_link(id)
+    query = "delete from links where id=#{id};"
+    @db.execute query
+    arr = get_links.collect do |link|
+      if link[0] > id
+        link[0]
+      end
+    end
+    arr.compact.each do |i|
+      update_link_id i
+    end
+  end
+
+  def add_hint(hint)
+    query = "insert or replace into hints values ('#{hint}');"
+    @db.execute query
+  end
+
+  def get_hints
+    query = "select * from hints;"
     @db.execute query
   end
 
